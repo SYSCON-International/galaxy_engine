397d7cac4eb1d75c3fd23517fb271bf316884548
tborkholder <64434300+tborkholder@users.noreply.github.com>
2025-07-11 13:05:45 -0400

    Fixed some of the changes needed

--- documentation_app/views/doc_section_view.py
+++ documentation_app/views/doc_section_view.py
@@ -5,7 +5,7 @@ from django.http import Http404
 
 def doc_section(request, section):
     section = section.lower()
-    template_path = f'galaxy_engine/partials/{section}.html'
+    template_path = f'documentation_app/partials/{section}.html'
 
     try:
         return render(request, template_path)
